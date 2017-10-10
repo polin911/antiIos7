37388b489627cc16d62c05f572654a64b02a7e5e
polin911 <polin911@mail.ru>
2017-10-10 15:38:18 +0300

    imagePicker

--- antiIos7/Global.swift
+++ antiIos7/Global.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import JSQMessagesViewController
 var chatMesArray :[ChatMessage]  = []
 var usersArray   :[String]       = []
 
--- antiIos7/JSQMesVC.swift
+++ antiIos7/JSQMesVC.swift
@@ -9,14 +9,22 @@
 import Foundation
 import JSQMessagesViewController
 import PubNub
+import UIKit
+import AVKit
+import MobileCoreServices
+//import
 
-class JSQMesVC: JSQMessagesViewController, PNObjectEventListener {
+class JSQMesVC: JSQMessagesViewController, PNObjectEventListener, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    let picker = UIImagePickerController()
+    
+    @IBOutlet var stickersCollection: UICollectionView!
+    
     var messageModel = [MesJSQ]()
     var mesModelJSQ = [JSQMessage]()
     var appDel = UIApplication.shared.delegate as! AppDelegate
 
-    var phForJSq = JSQPhotoMediaItem(image: UIImage(named: imgSticker))
+   // var phForJSq = JSQPhotoMediaItem(image: UIImage(named: imgSticker))
     ///
     var bubble = JSQMessagesBubbleImageFactory()
     var avatars = Dictionary<String, UIImage>()
@@ -26,6 +34,8 @@ class JSQMesVC: JSQMessagesViewController, PNObjectEventListener {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        picker.delegate = self
+       // collectionUpload()
         self.title = chan
         self.senderId = userName
         self.senderDisplayName = userName
@@ -38,9 +48,11 @@ class JSQMesVC: JSQMessagesViewController, PNObjectEventListener {
         self.collectionView.register(UINib(nibName: "messageViewOutgoingCell", bundle: nil), forCellWithReuseIdentifier: "outgoingCell")
     }
     
+    
     override func viewWillAppear(_ animated: Bool) {
         initPubNub()
-        updateTableview()
+       // updateTableview()
+        updateHistory()
     }
     override func viewDidAppear(_ animated: Bool) {
         
@@ -48,7 +60,14 @@ class JSQMesVC: JSQMessagesViewController, PNObjectEventListener {
         self.collectionView.collectionViewLayout.springinessEnabled = true
 
     }
+
     
+    ////Collection
+    func collectionUpload() {
+        
+        stickersCollection.delegate = self
+        stickersCollection.dataSource = self
+    }
     ///PubNub
     func initPubNub(){
         let appDel = UIApplication.shared.delegate! as! AppDelegate
@@ -94,157 +113,120 @@ class JSQMesVC: JSQMessagesViewController, PNObjectEventListener {
         print("**********deviceToken is **** \(deviceToken)")
     }
     
-    func parseJson(_ anyObj:AnyObject) -> [MesJSQ]{
-        
-        var list:[MesJSQ] = []
-        
-        if  anyObj is [AnyObject] {
-            
-            for jsonMsg in anyObj as! [AnyObject] {
-                let json = jsonMsg["message"] as? NSDictionary
-               
-                let usernameJson = (json?["username"] as AnyObject? as? String) ?? "" // to get rid of null
-                let textJson   = (json?["text"]  as AnyObject? as? String) ?? ""
-                let timeJson   = (json?["time"]  as AnyObject? as? String) ?? ""
-                let imgJson    = (json?["image"] as AnyObject as? String) ?? ""
-                let imgStickerJ  = (json?["stickers"] as AnyObject? as? String) ?? ""
-                
-                list.append(MesJSQ(username: usernameJson, textMes: textJson, time: timeJson, image: imgJson, imgSticker: imgStickerJ))
-                //pic
-                imgSticker = imgStickerJ
-                
-            }
-            collectionView.reloadData()
-            
-        }
-        
-        return list
-        
-    }
     func updateHistory(){
         
         let appDel = UIApplication.shared.delegate! as! AppDelegate
         
         appDel.client?.historyForChannel(chan, start: nil, end: nil, includeTimeToken: true, withCompletion: { (result, status) in
             print("!!!!!!!!!Status: \(result)")
-            
-            
-            chatMesArray2 = self.parseJson(result!.data.messages as AnyObject)
+         //   chatMesArray2 = self.parseJson(result!.data.messages as AnyObject)
             self.mesModelJSQ = self.parseJsonforJSqMes(result?.data.messages as AnyObject)
-            self.mesModelJSQ = self.parseJsonforJSqMedia(result?.data.messages as AnyObject)
-            self.updateTableview()
-            
+           // self.mesModelJSQ = self.parseJsonforJSqMedia(result?.data.messages as AnyObject)
+            self.collectionView.reloadData()
+            self.finishReceivingMessage()
+          
         })
     }
-    func updateTableview(){
-        self.collectionView.reloadData()
-        if self.collectionView.contentSize.height > self.collectionView.frame.size.height {
-            collectionView.scrollToItem(at: IndexPath(row: chatMesArray2.count - 1, section: 0), at: UICollectionViewScrollPosition.bottom, animated: true)
-        }
-    }
-    func updateChat(){
-        collectionView.reloadData()
-        
-        let numberOfSections = collectionView.numberOfSections
-        let numberOfRows = collectionView.numberOfItems(inSection: numberOfSections-1)
-        
-        if numberOfRows > 0 {
-            let indexPath = IndexPath(row:numberOfRows - 1, section:numberOfSections - 1)
-            
-            //IndexPath(forRow: numberOfRows-1, inSection: (numberOfSections-1))
-            collectionView.scrollToItem(at: indexPath, at: .bottom,
-                                      animated: true)
-        }
-    }
+
     func client(_ client: PubNub, didReceiveMessage message: PNMessageResult) {
         print("******didReceiveMessage*****")
         print("from client!!!!!!!!!!!!!!!!!!!!!!!\(message.data)")
         print("*******UUID from message IS \(message.uuid)")
         
         let stringData  = message.data.message as? NSDictionary
-        
-        
+    
         let stringName    = stringData?["username"] as? String ?? ""
         let stringText    = stringData?["text"] as? String ?? ""
         let stringTime    = stringData?["time"] as? String ?? ""
         let stringImg     = stringData?["image"] as? String ?? ""
         let stringSticker = stringData?["stickers"] as? String ?? ""
         
-        
-        let newMessage = MesJSQ(username: stringName, textMes: stringText, time: stringTime, image: stringImg, imgSticker: stringSticker)
-        chatMesArray2.append(newMessage)
-        
         //JSQ Pic
-//        let imgForJSq = UIImage(named: stringSticker)
-//        let phoForJSQ = JSQPhotoMediaItem(image: imgForJSq)
+        let imgForJSq = UIImage(named: stringSticker)
+        let phoForJSQ = JSQPhotoMediaItem(image: imgForJSq)
 //        
         let newMes = JSQMessage(senderId: stringName, displayName: stringName, text: stringText)
-//        let newM = JSQMessage(senderId: stringName, displayName: stringName, media: phoForJSQ)
-       mesModelJSQ.append(newMes!)
-                updateChat()
+        let newM = JSQMessage(senderId: stringName, displayName: stringName, media: phoForJSQ)
+        
+       // messageModel.append(MesJSQ(username: stringName, textMes: stringText, time: stringTime, image: stringImg, imgSticker: stringSticker))
+        mesModelJSQ.append(newM!)
+       // collectionView.reloadData()
+        finishReceivingMessage()
     }
     func getTime() -> String{
         let currentDate = Date()  // -  get the current date
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "hh:mm a" //format style to look like 00:00 am/pm
         let dateString = dateFormatter.string(from: currentDate)
-        
-        
         return dateString
     }
-    //////
+    ////// PickerView
     
-
+    private func chooseMedia(type: CFString) {
+        picker.mediaTypes = [type as String]
+        present(picker, animated: true, completion: nil)
+    }
+    
+    override func didPressAccessoryButton(_ sender: UIButton!) {
+        let alertAction = UIAlertController(title: "загрузить фотографию", message: "Твое фото", preferredStyle:.actionSheet)
+        let cancel = UIAlertAction(title: "отменить", style: .cancel, handler: nil)
+        let photos = UIAlertAction(title: "Фото", style: .default, handler: { (alert: UIAlertAction) in
+            self.chooseMedia(type: kUTTypeImage)
+            
+        })
+        let videos = UIAlertAction(title: "Видео", style: .default, handler: { (alert: UIAlertAction) in
+            self.chooseMedia(type: kUTTypeVideo)
+        })
+        alertAction.addAction(videos)
+        alertAction.addAction(photos)
+        alertAction.addAction(cancel)
+        present(alertAction, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let pic = info [UIImagePickerControllerOriginalImage] as? UIImage {
+            let img = JSQPhotoMediaItem(image: pic)
+            self.mesModelJSQ.append(JSQMessage(senderId: senderId, displayName: senderDisplayName, media: img))
+        } else if let video = info [UIImagePickerControllerMediaURL] as? URL {
+            
+        }
+        self.dismiss(animated: true, completion: nil)
+        collectionView.reloadData()
+    }
+    
+  //////JSQ
     override func didPressSend(_ button: UIButton!, withMessageText text: String!, senderId: String!, senderDisplayName: String!, date: Date!) {
         
         //for PubNub
-        let pubChat = MesJSQ(username: senderDisplayName, textMes: text, time: getTime(), image: "", imgSticker: imgSticker)
+        let pubChat = MesJSQ(username: senderDisplayName, textMes: text, time: getTime(), image: "", imgSticker: "")
         let newDict = chatMessageToDictionary(pubChat)
         appDel.client?.publish(newDict, toChannel: chan, compressed: true, withCompletion: nil)
        // messageModel.append(pubChat)
        // updateTableview()
         
         //For JSq
-        let mes = JSQMessage(senderId: senderId, senderDisplayName: senderDisplayName, date: date, text: text)
-      //  mesModelJSQ.append(mes!)
+        let mes = JSQMessage(senderId: senderId, displayName: senderDisplayName, text: text)
+        mesModelJSQ.append(mes!)
+        collectionView.reloadData()
         finishSendingMessage()
     }
     
+    
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        let message = self.mesModelJSQ[indexPath.item]        
-       
+            let message = self.mesModelJSQ[indexPath.item]
             let cell = super.collectionView(collectionView, cellForItemAt: indexPath) as! JSQMessagesCollectionViewCell
-//            if message.isMediaMessage {
-//                cell.mediaView = message.media as! UIView!
-//                return cell
-//
-//            } else {
-//                return cell
-//        }
         
             return cell
         }
-    
-    
-//     func collectionView(collectionView: JSQMessagesCollectionView!, bubbleImageViewForItemAtIndexPath indexPath: NSIndexPath!) -> UIImageView! {
-//        let message = mesModelJSQ[indexPath.item]
-//
-//
-//        if message.senderId == self.senderId {
-//            return UIImageView(image: outgoingBubbleImageView.image, highlightedImage: outgoingBubbleImageView.highlightedImage)
-//        }
-//
-//        return UIImageView(image: incomingBubbleImageView.image, highlightedImage: incomingBubbleImageView.highlightedImage)
-//    }
+
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return mesModelJSQ.count
-        // return messageModel.count
+   
     }
     
     override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageDataForItemAt indexPath: IndexPath!) -> JSQMessageData! {
-        //mesModelJSQ[indexPath.item].text == messageModel[indexPath.row].textMes
-        
+
         return mesModelJSQ[indexPath.item]
     }
     
@@ -259,8 +241,6 @@ class JSQMesVC: JSQMessagesViewController, PNObjectEventListener {
         }
     }
     override func collectionView(_ collectionView: JSQMessagesCollectionView!, avatarImageDataForItemAt indexPath: IndexPath!) -> JSQMessageAvatarImageDataSource! {
-        
-        
         return nil
     }
     
@@ -278,8 +258,8 @@ class JSQMesVC: JSQMessagesViewController, PNObjectEventListener {
 }
 extension JSQMesVC {
     func parseJsonforJSqMes(_ anyObj:AnyObject) -> [JSQMessage] {
-        var list: [JSQMessage] = []
-         if  anyObj is [AnyObject] {
+        var list : [JSQMessage] = []
+        if  anyObj is [AnyObject] {
         for jsonMsg in anyObj as! [AnyObject] {
             let json = jsonMsg["message"] as? NSDictionary
             
@@ -294,7 +274,7 @@ extension JSQMesVC {
             let imgForJSq = UIImage(named: imgStickerJ)
             let phoForJSQ = JSQPhotoMediaItem(image: imgForJSq)
             
-            imgSticker = imgStickerJ
+           // imgSticker = imgStickerJ
            // mesModelJSQ.append(JSQMessage(senderId: usernameJson, displayName: usernameJson, media: phoForJSQ))
         list.append(JSQMessage(senderId: usernameJson, displayName: usernameJson, text: textJson))
           // list.append(JSQMessage(senderId: usernameJson, displayName: usernameJson, media: phoForJSQ))
@@ -309,7 +289,7 @@ extension JSQMesVC {
     }
     
     func parseJsonforJSqMedia(_ anyObj:AnyObject) -> [JSQMessage] {
-        var list: [JSQMessage] = []
+       var list : [JSQMessage] = []
         if  anyObj is [AnyObject] {
             for jsonMsg in anyObj as! [AnyObject] {
                 let json = jsonMsg["message"] as? NSDictionary
@@ -325,7 +305,7 @@ extension JSQMesVC {
                 let imgForJSq = UIImage(named: imgStickerJ)
                 let phoForJSQ = JSQPhotoMediaItem(image: imgForJSq)
                 
-                imgSticker = imgStickerJ
+               // imgSticker = imgStickerJ
                // mesModelJSQ.append(JSQMessage(senderId: usernameJson, displayName: usernameJson, media: phoForJSQ))
                // list.append(JSQMessage(senderId: usernameJson, displayName: usernameJson, text: textJson))
                  list.append(JSQMessage(senderId: usernameJson , displayName: usernameJson, media: phoForJSQ))
