4c2ae161c388121cb9a5671d2007926741f8c484
polin911 <polin911@mail.ru>
2017-11-23 19:11:47 +0300

    SwiftyCam

--- Podfile
+++ Podfile
@@ -1,5 +1,5 @@
 # Uncomment the next line to define a global platform for your project
- platform :ios, ‘8.0’
+ platform :ios, ‘8’
 
 target 'antiIos7' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
@@ -9,7 +9,7 @@ target 'antiIos7' do
 pod 'PubNub', '~> 4'
 pod 'JSQMessagesViewController'
 pod 'Parse'
-pod 'SDWebImage'
+pod 'SDWebImage', '~> 4.0'
 pod "SwiftyCam"
 
 
--- Pods/Target Support Files/Pods-antiIos7/Pods-antiIos7-frameworks.sh
+++ Pods/Target Support Files/Pods-antiIos7/Pods-antiIos7-frameworks.sh
@@ -95,6 +95,7 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Parse/Parse.framework"
   install_framework "$BUILT_PRODUCTS_DIR/PubNub/PubNub.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SDWebImage/SDWebImage.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/SwiftyCam/SwiftyCam.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Bolts/Bolts.framework"
@@ -103,6 +104,7 @@ if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Parse/Parse.framework"
   install_framework "$BUILT_PRODUCTS_DIR/PubNub/PubNub.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SDWebImage/SDWebImage.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/SwiftyCam/SwiftyCam.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
